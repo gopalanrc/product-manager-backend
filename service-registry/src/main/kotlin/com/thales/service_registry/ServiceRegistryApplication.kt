7f5b674881e0739b1f6dec47774c1078cb54ab8c
Rajamani Chandrasekaran Gopalan <gopalanrc@gmail.com>
2024-12-07 23:53:11 +0530

    Service Registry Configuration
    Configured the service registry with the port and other required info.

--- service-registry/src/main/kotlin/com/thales/service_registry/ServiceRegistryApplication.kt
+++ service-registry/src/main/kotlin/com/thales/service_registry/ServiceRegistryApplication.kt
@@ -2,10 +2,12 @@ package com.thales.service_registry
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class ServiceRegistryApplication
 
 fun main(args: Array<String>) {
-	runApplication<ServiceRegistryApplication>(*args)
+    runApplication<ServiceRegistryApplication>(*args)
 }
